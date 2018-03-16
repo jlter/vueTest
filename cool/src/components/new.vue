da605f88a258102cef76297df4538ea36ff48978
jliter_zhu <zhuhaoyuJliter@163.com>
2018-03-16 15:00:53 +0800

    新增组件
    新闻组件

---
+++ cool/src/components/new.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <p>新增组件，测试如何提交以及coding fork</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
